af6a50700ac3f621c339c2ad8fe4e0d22410fe05
crearwebdesign <oscar10034@gmail.com>
2023-09-23 14:58:32 -0400

    subirtodo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { RegisterComponent } from './registro/register/register.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagefoundComponent,
+    PrincipalComponent,
+    BreadcrumsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    RegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
